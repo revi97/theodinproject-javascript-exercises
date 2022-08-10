e8336a86ac64a1b3763e74f79c4687e98e94b87b
Revanth <revanthhs@protonmail.com>
2022-08-11 02:55:39 +0530

    Solved 01 and 02 modules
    Solved the test cases of both 01 and 02 test modules.
    02_repeatstring module all test cases have been checked and tests are
    passed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(a, times){
+    if(times<0){
+        return 'ERROR';
+    }
+    else if(times === 0 ){
+        return '';
+    }
+    else{
+        return a.repeat(times);
+    }
+  
+    
 };
 
 // Do not edit below this line

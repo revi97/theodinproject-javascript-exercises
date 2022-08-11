f7fe79ced7c55feac12583ec88db72e0bd25b3a1
Revanth <revanthhs@protonmail.com>
2022-08-11 23:20:15 +0530

    Completed next two tests
    Completed both leapYears and tempConversion as a part of The Odin
    Project Fundamentals Part 4 assignments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,30 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+  /*  if(year% 100 == 0 && year % 400 == 0 )
+    {
+        return true;
+    }
+    else if(year % 4 == 0 && year % 100 == 0){
+        return false;
+    }
+    else if(year % 4 == 0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+    */
+
+    //reduced the above solution to below simple solution
+
+    if (year % 4 === 0 && (year % 400 === 0 || year % 100 != 0))
+    {
+        return true;
+
+    }
+    else 
+        return false;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temps) {
+  
+  f = ((temps - 32) *(5/9) );
+  n =Math.round(f * 10)/ 10;
+  return n;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  c= (temp * (9/5) + 32);
+  n = Math.round(c * 10)/ 10;
+  return n;
 };
 
 // Do not edit below this line

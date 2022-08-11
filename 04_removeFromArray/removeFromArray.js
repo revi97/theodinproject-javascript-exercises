11e3ba1afbdeed42e3bc6c91a01d64b4e51a2118
Revanth <revanthhs@protonmail.com>
2022-08-11 21:13:24 +0530

    Solved reverse, remove and sumAll problems
    Wrote the conditions to solve the given tests and passed all the tests
    for each of the folders.
    Passed all test conditions for the first 5.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const arr = args[0];
+    const array = new Array();
+    arr.forEach((values) =>  //Should not forget the braces to be added properly here for the function
+    {
+        if(!args.includes(values)) 
+        {
+//If the there are any arguments passed in the function, we will check if those are not present in the array.
+//If they are not present then we will be pushing that value to the new array.
+            array.push(values);
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    //Psuedocode
+    //Get two integers where a is the starting value and b is the ending value of the numbers to be summed up
+    //Try to do a loop where a is assigned to i and b+1 will be the length of the loop
+    // Add i=+i and return it as sum
 
+    let sum = 0;
+    if(a<0 || b< 0){
+        return 'ERROR';
+    }
+    else if(Array.isArray(b))
+    {
+        return 'ERROR';
+    }
+    else if(typeof b === 'string')
+    {
+        return 'ERROR';
+    }
+    else{
+        if(a>b){
+            let c = a;
+            a = b;
+            b =c;    
+        }
+        for(i=a; i<=b;i++){
+            sum = sum+i;
+        }
+        return sum;
+    }
+    
+        
 };
 
 // Do not edit below this line
